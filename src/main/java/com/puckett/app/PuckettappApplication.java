5cda4ca6d95def40f02e6da7749674d46c70576b
saucekode <saucekoder@gmail.com>
2021-05-10 14:41:55 +0100

    puckett starter files

---
+++ src/main/java/com/puckett/app/PuckettappApplication.java
@@ -0,0 +1,13 @@
+package com.puckett.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuckettappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuckettappApplication.class, args);
+	}
+
+}
